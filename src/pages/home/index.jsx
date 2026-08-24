d2926a9f93b19058a220df654d61bd3f83496d62
Geovanny Neves Liberato <geovanny.liberato@inventsoftware.com.br>
2026-08-24 18:42:18 -0300

    Criado layout da tela Home

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -1,8 +1,28 @@
-import { Header } from '../../components/Header';
-
+import { Header } from "../../components/Header";
+import { Button } from "../../components/Button";
+import { Container, TextContent, Title, TitleHighlight } from "./style";
+import banner from "../../assets/banner.png";
 
 const Home = () => {
-    return (<Header>Home</Header>)
+    return (<>
+        <Header />
+        <Container>
+            <div>
+                <Title>
+                    <TitleHighlight>
+                        Implemente <br />
+                    </TitleHighlight>
+                    o seu futuro global agora!
+                </Title>
+                <TextContent>
+                    Domine as tecnologias utilizadas pelas empresas mais inovadoras do mundo e encare seu novo
+                    desafio profissional, evoluindo em comunidade com os melhores experts.
+                </TextContent>
+                <Button title="Começar agora" variant="secondary" onClick={ () => null } />
+            </div>
+            <div><img src={ banner } alt="Imagem principal" /></div>
+        </Container>
+    </>)
 }
 
 export { Home };
